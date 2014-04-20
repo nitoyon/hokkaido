71f31079f321d38ead40f4a1c5209ff589b26b7a
nitoyon <nitoyon@gmail.com>
2014-04-21 00:57:46 +0900

    polygon-edit: improve grunt default task (watch, build and reload)

--- html5/polygon-edit/Gruntfile.js
+++ html5/polygon-edit/Gruntfile.js
@@ -3,6 +3,18 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      src: {
+        files: ['src/*.js', 'Gruntfile.js'],
+        tasks: ['jshint', 'concat']
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js']
+    },
+
     concat: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -11,11 +23,19 @@ module.exports = function(grunt) {
         src: ['src/main.js', 'src/mode.js', 'src/model.js', 'src/view.js'],
         dest: 'dist/<%= pkg.name %>.js'
       }
+    },
+
+    livereloadx: {
+      static: true,
+      dir: 'dist/'
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('livereloadx');
 
-  // Default task(s).
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['livereloadx', 'build', 'watch']);
+  grunt.registerTask('build', ['jshint', 'concat']);
 };
